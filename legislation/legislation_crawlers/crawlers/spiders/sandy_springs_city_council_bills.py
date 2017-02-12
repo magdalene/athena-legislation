d15d629cd613a13c726b8bf753da40545f1858bf
Darla Magdalene Shockley <hecateluna@gmail.com>
2017-02-12 17:28:37 +0100

    WIP - Sandy Springs bill crawler

---
+++ legislation/legislation_crawlers/crawlers/spiders/sandy_springs_city_council_bills.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import io
+from datetime import datetime
+from urllib.parse import urljoin
+
+from pdfminer.pdfinterp import PDFResourceManager, process_pdf
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+
+
+from scrapy.spiders import CrawlSpider
+
+import os
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "legislation.settings")
+django.setup()
+from legislation_models.models import *
+SANDY_SPRINGS = Place.objects.get(name='Sandy Springs')
+SANDY_SPRINGS_CITY_COUNCIL = LegislativeBody.objects.get(place=SANDY_SPRINGS)
+
+
+class SandySpringsCityCouncilOrdinanceSpider(CrawlSpider):
+    name = "sandy_springs_bills"
+    start_urls = [bill.link
+                  for bill in Bill.objects.filter(status=Bill.STATUS_NEEDS_UPDATE, legislative_body=SANDY_SPRINGS_CITY_COUNCIL)
+                  if bill.link is not None]
+
+    def parse(self, response):
+        filename = '/tmp/leg_%s.pdf' % datetime.now().strftime('%Y%m%d%H%S%f')
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        fp = io.open(filename, 'rb')
+        out_filename = filename.replace('.pdf', '.txt')
+        outfp = io.open(out_filename, 'wt', errors='ignore')
+        rsrcmgr = PDFResourceManager(caching=True)
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, outfp, laparams=laparams)
+        pagenos = set()
+
+        process_pdf(rsrcmgr, device, fp, pagenos, maxpages=10, password='',
+                    caching=True, check_extractable=True)
+
+        fp.close()
+        outfp.close()
+        with open(out_filename) as f:
+            text = f.read()
+        # TODO: have a correct bill_type, maybe from the meeting page?
+        bill_type = 'Ordinance'
+
+        # TODO: history
+        # TODO: sponsor(s) -- not sure how this is going to work here, have to read more
+        # actual texts
+        bill = Bill.objects.get(link=response.url, legislative_body=SANDY_SPRINGS_CITY_COUNCIL)
+        bill.bill_type = bill_type
+        bill.text = text
+        bill.status = Bill.STATUS_UP_TO_DATE
+        bill.save()
